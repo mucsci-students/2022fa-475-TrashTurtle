b3c738a43d138cf0011549710073d5ab2891ee48
RjCor <93812563+RjCor@users.noreply.github.com>
2022-09-25 17:40:37 -0400

    Initial Commit

---
+++ Assets/Scripts/BobbertV2/Platformer2DUserControl.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+namespace UnityStandardAssets._2D
+{
+    [RequireComponent(typeof (PlatformerCharacter2D))]
+    public class Platformer2DUserControl : MonoBehaviour
+    {
+        private PlatformerCharacter2D m_Character;
+        private bool m_Jump_down;
+        private bool m_Jump_button;
+
+        private void Awake()
+        {
+            m_Character = GetComponent<PlatformerCharacter2D>();
+        }
+
+
+        private void Update()
+        {
+            if (!m_Jump_down)
+            {
+                // m_Jump_down when jump is pressed
+                m_Jump_down = Input.GetButtonDown("Jump");
+                // m_Jump_button duration jump is pressed
+                m_Jump_button = Input.GetButton("Jump");
+            }
+        }
+
+
+        private void FixedUpdate()
+        {
+            // Read the inputs.
+            bool shield = Input.GetKey(KeyCode.S);
+            float h = Input.GetAxis("Horizontal");
+            // Pass all parameters to the character control script.
+            m_Character.Move(h, shield, m_Jump_down, m_Jump_button);
+            m_Jump_down = false;
+            m_Jump_button = false;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# 2022fa-475-TrashTurtle
