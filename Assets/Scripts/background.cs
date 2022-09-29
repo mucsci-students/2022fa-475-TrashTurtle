10d16638d06539cf53f58b4a1177345ec84785ea
Christian <shep@shep.localdomain>
2022-09-28 20:40:46 -0400

    added new scene

---
+++ Assets/Scripts/background.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class background : MonoBehaviour
+{
+    public Transform mainCam;
+    public Transform midBG;
+    public Transform sideBG;
+  
+    public float length = 30.23f;
+    void Update()
+    {
+        if(mainCam.position.x > midBG.position.x){
+            sideBG.position = midBG.position + Vector3.right * length;
+        }
+        if(mainCam.position.x < midBG.position.x){
+            sideBG.position = midBG.position + Vector3.left * length;
+        }
+    }
+
+
+
+}
