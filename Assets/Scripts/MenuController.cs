b251f48244a6e1debf4f98bdae28cc8bddec4080
Samantha Noggle <44234583+astruxie@users.noreply.github.com>
2022-10-01 11:45:11 -0400

    added volume slider
    play is functional, sends you to level1. load game is functional it just needs the scenes to exist to send you there. settings works but just for volume. quit should work.

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -3,20 +3,48 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
-
+using TMPro;
 
 
 public class MenuController : MonoBehaviour
 {
+    [Header("Volume Setting")]
+    [SerializeField] private TMP_Text volumeTextValue = null;
+    [SerializeField] private Slider volumeSlider = null;
+
+
+    [Header("Confirmation")]
+    [SerializeField] private GameObject confirmationPrompt = null;
+
     public string _newGameLevel;
 
     public void PlayButton()
     {
         SceneManager.LoadScene(_newGameLevel);
     }
+    
     public void QuitButton()
     {
         Application.Quit();
+    }
+
+    public void setVolume(float volume)
+    {
+        AudioListener.volume = volume;
+        volumeTextValue.text = volume.ToString("0");
+    }
+
+    public void applySettings () 
+    {
+        PlayerPrefs.SetFloat("masterVolume", AudioListener.volume);
+        StartCoroutine(ConfirmationBox());
+    }
+
+    public IEnumerator ConfirmationBox()
+    {
+        confirmationPrompt.SetActive(true);
+        yield return new WaitForSeconds(2);
+        confirmationPrompt.SetActive(false);
     }
 
 }
