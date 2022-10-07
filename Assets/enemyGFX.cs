3cbd4bd33ebb7497a73618091d770ce18f3dacae
Samantha Noggle <44234583+astruxie@users.noreply.github.com>
2022-10-07 15:17:02 -0400

    Level1 Aesthetics

--- Assets/enemyGFX.cs
+++ Assets/enemyGFX.cs
@@ -13,10 +13,10 @@ public class enemyGFX : MonoBehaviour
     {
       
         if(aiPath.desiredVelocity.x >= 0.01f ){
-            transform.localScale = new Vector3(-1f, 1f, 1f);
+            transform.localScale = new Vector3(.4f, .4f, 0f);
         }
         else if( aiPath.desiredVelocity.x <= -0.01f){
-            transform.localScale = new Vector3(1f, 1f, 1f);
+            transform.localScale = new Vector3(.4f, .4f, 0f);
         }
     }
 }
