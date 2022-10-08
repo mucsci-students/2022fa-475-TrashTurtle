1bef9ca1423e30955346c24e40da489c36b26c9f
RjCor <93812563+RjCor@users.noreply.github.com>
2022-10-08 13:32:52 -0400

    He flash red now when he get hit

--- Assets/Scripts/BobbertV2/Bobbert/BobbertHealth.cs
+++ Assets/Scripts/BobbertV2/Bobbert/BobbertHealth.cs
@@ -7,29 +7,35 @@ public class BobbertHealth : MonoBehaviour
 {
     public int maxHealth = 6;
     public int health;
-    // public SpriteRenderer sprite;
-    
+    SpriteRenderer spriteRenderer;
+    Color defaultColor;
+    bool isHit = false;
+    public float colorChangeTimer;
+
     // Start is called before the first frame update
     void Start()
     {
         health = maxHealth;
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        defaultColor = spriteRenderer.color;
     }
 
     // Other enemies call this function
     // Also will be used by scissors !
     public void UpdateHealth(int mod)
     {
+        // If Bobbert's health is going down:
+        if(health + mod <= health)
+        {
+            if(!isHit)
+            {
+                isHit = true;
+                // Idk how to explain Coroutine. But we can run a function as if it were in Update()
+                StartCoroutine("Flash");
+            }
+        }
+
         health += mod;
-
-        // sprite.color = Color.red;
-
-        // float emission = Mathf.PingPong (Time.time, 1.0f);
-        // Color baseColor = Color.yellow; //Replace this with whatever you want for your base color at emission level '1'
- 
-        // Color finalColor = baseColor * Mathf.LinearToGammaSpace (emission);
- 
-        // Color.finalColor;
-
 
         if (health > maxHealth)
         {
@@ -40,10 +46,38 @@ public class BobbertHealth : MonoBehaviour
             PlayerDied();
         }
     }
-
+ 
     private void PlayerDied()
     {
         LevelManager.instance.GameOver();
         gameObject.SetActive(false);
+    }
+
+    IEnumerator Flash(){
+        
+        // Duration to flash Bobbert.
+        float duration = 0.5f;
+        while(duration > 0)
+        {
+            //bAsE cAsE fOr nO iNFinIte L00p$
+            duration -= Time.deltaTime;
+
+            // It make it look flashy.
+            float emission = Mathf.PingPong(2 * Time.time, 1f);
+            
+            // Color we're changing to
+            Color baseColor = Color.red;
+            // It make it look flashy 2.
+            Color finalColor = baseColor * Mathf.LinearToGammaSpace(emission);
+            
+            // Set new color.
+            spriteRenderer.color = finalColor;
+
+            // Coroutine uses this yield return thing. I understand my code.
+            yield return new WaitForSeconds(colorChangeTimer);
+            // Change him back to his normal color
+            spriteRenderer.color = defaultColor;
+            isHit = false;
+        }
     }
 }
